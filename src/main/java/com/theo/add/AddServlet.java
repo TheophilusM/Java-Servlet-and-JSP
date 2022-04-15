a35d8ed961836643f3b5f5afa1c1f2f2021004b6
TheophilusM <theotakumug@gmail.com>
2022-04-15 02:53:44 +0200

    Use the res object in servlet

--- src/main/java/com/theo/add/AddServlet.java
+++ src/main/java/com/theo/add/AddServlet.java
@@ -1,15 +1,21 @@
 package com.theo.add;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class AddServlet extends HttpServlet {
-	public void service(HttpServletRequest req, HttpServletResponse res) {
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		int a = Integer.parseInt(req.getParameter("num1"));
 		int b = Integer.parseInt(req.getParameter("num2"));
 		
 		int result = a + b;
 		System.out.println("Result: " + result);
+		
+		PrintWriter outObj =  res.getWriter();
+		outObj.println("Result: " + result);
 	}
 }
