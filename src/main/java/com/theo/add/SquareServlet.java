54f47ffcb895e8ffd17f81214e8288d6011041d7
TheophilusM <theotakumug@gmail.com>
2022-04-15 11:00:28 +0200

    utilizing cookies

--- src/main/java/com/theo/add/SquareServlet.java
+++ src/main/java/com/theo/add/SquareServlet.java
@@ -3,6 +3,7 @@ package com.theo.add;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,8 +12,9 @@ import javax.servlet.http.HttpSession;
 public class SquareServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		PrintWriter outObj =  res.getWriter();
-		HttpSession httpSession = req.getSession();
+		
 		/*
+		HttpSession httpSession = req.getSession();
 		outObj.println("Running another servlet");
 		System.out.println("Running another servlet");
 		outObj.println();
@@ -26,10 +28,22 @@ public class SquareServlet extends HttpServlet {
 		// get parameter for url redirect
 		int param = Integer.parseInt(req.getParameter("valueToPass"));
 		outObj.println("Parameter: " + param);
-		 */
 		
 		// get parameter for session
 		int param = (int) httpSession.getAttribute("valueToPass");
 		outObj.println("Session param: " + param);
+		
+		// can delete data from session using removeSession() method
+		 */
+		
+		// using cookie array req
+		int cookieValue = 0;
+		Cookie cookies[] = req.getCookies();
+		for(Cookie cookie : cookies) {
+			if (cookie.getName() == "valueToPass") {
+				cookieValue = Integer.parseInt(cookie.getValue());
+				outObj.println("Cookie param: " + cookieValue);
+			}
+		}
 	}
 }
