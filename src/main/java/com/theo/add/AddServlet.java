ece832e0ed6f55c2cdaa249f3c250baae0d9060c
TheophilusM <theotakumug@gmail.com>
2022-04-15 03:11:23 +0200

    Use doPost method

--- src/main/java/com/theo/add/AddServlet.java
+++ src/main/java/com/theo/add/AddServlet.java
@@ -8,7 +8,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class AddServlet extends HttpServlet {
+	//	work with post or get method
+	/*
 	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		int a = Integer.parseInt(req.getParameter("num1"));
+		int b = Integer.parseInt(req.getParameter("num2"));
+		
+		int result = a + b;
+		System.out.println("Result: " + result);
+		
+		PrintWriter outObj =  res.getWriter();
+		outObj.println("Result: " + result);
+	}
+	*/
+	// only work with post request
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		int a = Integer.parseInt(req.getParameter("num1"));
 		int b = Integer.parseInt(req.getParameter("num2"));
 		
